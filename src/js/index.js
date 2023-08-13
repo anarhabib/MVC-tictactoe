0ca72b2e86c0e6dd02f0513c3a56cd537643b98d
Anar Habib <anarhabibullayev3@gmail.com>
2023-08-13 19:21:08 +0400

    bugs fixed

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,14 @@ import Model from './model.js'
 import View from './view.js'
 import Controller from './controller.js'
 
+import '../style.css'
+
 function initialize() {
     const model = new Model();
     const view = new View();
     const controller = new Controller(model, view);
 
-    controller.init(view);
+    controller.init();
 }
 
 window.addEventListener('DOMContentLoaded', initialize);
